4e0e2ce2373f2efa8375b41d9a0306ee1eed392c
bo0tzz <git@bo0tzz.me>
2022-12-26 21:33:52 +0100

    Fix config path

--- config/runtime.exs
+++ config/runtime.exs
@@ -8,6 +8,6 @@ chats_allowlist =
   System.get_env("CHATS_ALLOWLIST", "")
   |> String.split(",")
 
-config :image_bot,
+config :mayhem_chatbot,
   bot_token: System.fetch_env!("BOT_TOKEN"),
   chats_allowlist: chats_allowlist
